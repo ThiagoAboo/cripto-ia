b100d51e07f7fc779fdd50013c89b7030e7d5003
thiago.vieira <thiago.vieira@dbccompany.com.br>
2026-03-26 19:25:14 -0300

    patch

--- patch/scripts/test-all.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-bash scripts/test-backend.sh
-bash scripts/test-frontend.sh
--- patch/scripts/test-backend.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-cd backend
-npm install
-npm test
